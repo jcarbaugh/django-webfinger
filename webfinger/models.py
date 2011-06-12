8f65b0af993af413e8688c2b75517c0bc8f30b35
Paul Osman <paul@eval.ca>
2011-06-11 23:23:06 -0400

    Don't use django.contrib.sites, it creates problems when running syncdb.

--- webfinger/models.py
+++ webfinger/models.py
@@ -1,17 +1,19 @@
-from django.db import models
 from django.conf import settings
-from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
+
 from webfinger.rel import LRDD
 from xrd import Link
+
 import urllib
 import wellknown
 
 SECURE = getattr(settings, 'WEBFINGER_SECURE', False)
+DOMAIN = getattr(settings, 'WEBFINGER_DOMAIN', 'example.com')
+
 scheme = "https" if SECURE else "http"
 
 endpoint = urllib.unquote(reverse('webfinger_endpoint', args=(r'{uri}',)))
-uri_template = "%s://%s%s" % (scheme, Site.objects.get_current().domain, endpoint)
+uri_template = "%s://%s%s" % (scheme, DOMAIN, endpoint)
 
 webfinger_link = Link(rel=LRDD, template=uri_template)
 webfinger_link.titles.append('Resource Descriptor')
